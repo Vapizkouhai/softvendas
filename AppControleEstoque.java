5de1e20f3e0f3bba8ce9059b1466d3599d0a24c7
Vapizkouhai <60707840+Vapizkouhai@users.noreply.github.com>
2021-06-25 03:12:22 -0300

    Create AppControleEstoque.java

---
+++ AppControleEstoque.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softvendas;
+
+/**
+ *
+ * @author vapiz
+ */
+public class AppControleEstoque {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PainelPrincipal p = new PainelPrincipal();
+        p.executar();// TODO code application logic here
+    }
+    
+}
