b8527d936cfc938cd7fca7e51344eb573f8ca759
Vapizkouhai <60707840+Vapizkouhai@users.noreply.github.com>
2021-06-26 04:30:08 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,11 @@
+package softvendas;
+
+public class Cliente {
+    private String tipoDeDado;
+    private String identificacao;
+    private String nome;
+    private boolean ativo;
+    private int quantidadeDeCompras;
+    private int quantidadeItensComprados;
+    private float valorCompradoTotal;
+}
---
+++ Produto.java
@@ -0,0 +1,9 @@
+package softvendas;
+
+public class Produto {
+    private String codigo;
+    private String descricao;
+    private int quantidade;
+    private float valor;
+    private boolean disponivel;
+}
