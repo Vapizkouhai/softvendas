c884c4f69136c2b61f2a4c9e7ce70eb40a024681
Vapizkouhai <60707840+Vapizkouhai@users.noreply.github.com>
2021-07-11 03:06:05 -0300

    Update ListaDeCategorias.java

--- ListaDeCategorias.java
+++ ListaDeCategorias.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package softvendas;
 
-/**
- *
- * @author vapiz
- */
-public class ListaDeCategorias {
+public class ListaDeCategorias implements ControleDeDados{
+    public Categoria[] Categorias;
     
+    @Override
+    public void atualizarArquivo(){
+        
+    }
+    
+    @Override
+    public void atualizarSistema(){
+        
+    }
 }
