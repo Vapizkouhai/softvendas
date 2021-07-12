89d38cff0e309070d6a077e5bbfb49c3278b596a
Vapizkouhai <60707840+Vapizkouhai@users.noreply.github.com>
2021-07-12 04:54:11 -0300

    Update ListaDeCategorias.java
    Adição de atributo arquivo e implementação de leitura

--- ListaDeCategorias.java
+++ ListaDeCategorias.java
@@ -1,7 +1,12 @@
 package softvendas;
 
-public class ListaDeCategorias implements ControleDeDados{
-    public Categoria[] Categorias;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ListaDeCategorias extends GrupoDeObjetos implements ControleDeDados{
+    private Categoria[] categorias;
+    private final java.io.File arquivoCategorias = new java.io.File("categorias.txt");
     
     @Override
     public void atualizarArquivo(){
@@ -10,6 +15,19 @@ public class ListaDeCategorias implements ControleDeDados{
     
     @Override
     public void atualizarSistema(){
-        
+        if (arquivoCategorias.canRead()){
+            java.util.Scanner scannerReadInfo;
+            try {
+                categorias = new Categoria[numItensNoArquivo() - 1];
+                scannerReadInfo = new java.util.Scanner(arquivoCategorias);
+                scannerReadInfo.nextLine();
+                int itemAtual = 0;
+                while(scannerReadInfo.hasNext()){
+                    categorias[itemAtual] = new Categoria(scannerReadInfo.nextLine());
+                }    
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(RelatorioDeVendas.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
