587c73ce9ef6112c3458ae04859ee26019dc82b7
Vapizkouhai <60707840+Vapizkouhai@users.noreply.github.com>
2021-07-12 05:03:32 -0300

    Update RelatorioDeVendas.java
    Adição de atributo arquivo e implementação de leitura

--- RelatorioDeVendas.java
+++ RelatorioDeVendas.java
@@ -1,7 +1,12 @@
 package softvendas;
 
-public class RelatorioDeVendas implements ControleDeDados{
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class RelatorioDeVendas extends GrupoDeObjetos implements ControleDeDados{
     private Venda[] vendasRealizadas;
+    private final java.io.File arquivoVendas = new java.io.File("vendas.txt"); 
     
     @Override
     public void atualizarArquivo(){
@@ -10,6 +15,19 @@ public class RelatorioDeVendas implements ControleDeDados{
     
     @Override
     public void atualizarSistema(){
-        
+        if (arquivoVendas.canRead()){
+            java.util.Scanner scannerReadInfo;
+            try {
+                vendasRealizadas = new Venda[numItensNoArquivo() - 1];
+                scannerReadInfo = new java.util.Scanner(arquivoVendas);
+                scannerReadInfo.nextLine();
+                int itemAtual = 0;
+                while(scannerReadInfo.hasNext()){
+                    vendasRealizadas[itemAtual] = new Venda(scannerReadInfo.nextLine());
+                }    
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(RelatorioDeVendas.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
