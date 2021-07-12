7f02e5be187189340f3135bc5edb4f3a43b396d0
Vapizkouhai <60707840+Vapizkouhai@users.noreply.github.com>
2021-07-12 04:43:06 -0300

    Update Clientela.java
    Adição de atributo para arquivo e método para retorno de todas as identificações na lista, e implementação do método para leitura do arquivo

--- Clientela.java
+++ Clientela.java
@@ -1,7 +1,12 @@
 package softvendas;
 
-public class Clientela implements ControleDeDados{
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Clientela extends GrupoDeObjetos implements ControleDeDados{
     private Cliente[] clientesCadastrados;
+    private final java.io.File arquivoClientes = new java.io.File("clientes.txt");
     
     @Override
     public void atualizarArquivo(){
@@ -10,6 +15,27 @@ public class Clientela implements ControleDeDados{
     
     @Override
     public void atualizarSistema(){
-        
+        if (arquivoClientes.canRead()){
+            java.util.Scanner scannerReadInfo;
+            try {
+                clientesCadastrados = new Cliente[numItensNoArquivo() - 1];
+                scannerReadInfo = new java.util.Scanner(arquivoClientes);
+                scannerReadInfo.nextLine();
+                int itemAtual = 0;
+                while(scannerReadInfo.hasNext()){
+                    clientesCadastrados[itemAtual] = new Cliente(scannerReadInfo.nextLine());
+                }    
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(RelatorioDeVendas.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public String[] idsDeCliente(){
+        String[] todosOsIds = new String[clientesCadastrados.length - 1];
+        for (int i = 0; i <= todosOsIds.length - 1; ++i){
+            todosOsIds[i] = clientesCadastrados[i].getIdentificacao();
+        }
+        return todosOsIds;
     }
 }
