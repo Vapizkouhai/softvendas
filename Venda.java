0d61523531778dc4c7b59a6f0c194b43a7ddc314
Vapizkouhai <60707840+Vapizkouhai@users.noreply.github.com>
2021-07-12 05:04:26 -0300

    Update Venda.java
    Adição de construtor

--- Venda.java
+++ Venda.java
@@ -1,8 +1,12 @@
 package softvendas;
 
-public class Venda {
+public class Venda extends ElementoDeTransacaoRegistravel implements Printavel{
     private Cliente comprador;
     private Produto[] listaDeCompras;
     private float valorAPagar;
     private String dataEHoraDaTransacao;
+    
+    public Venda(String info){
+        
+    }
 }
