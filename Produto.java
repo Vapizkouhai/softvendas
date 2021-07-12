4dffd6f86a27b9bc4c88f8ce2c9974fd70a9dd45
Vapizkouhai <60707840+Vapizkouhai@users.noreply.github.com>
2021-07-12 05:02:25 -0300

    Update Produto.java
    Adição de construtor

--- Produto.java
+++ Produto.java
@@ -1,9 +1,14 @@
 package softvendas;
 
-public class Produto {
+public class Produto extends ElementoDeTransacaoRegistravel implements Printavel{
     private String codigo;
     private String descricao;
+    private Categoria categoriaDoProduto;
     private int quantidade;
     private float valor;
     private boolean disponivel;
+    
+    public Produto(String info){
+        
+    }
 }
