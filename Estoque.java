aa5905cd801bbceab7c09a0239632c9d63e3a769
Vapizkouhai <60707840+Vapizkouhai@users.noreply.github.com>
2021-07-12 04:50:07 -0300

    Update Estoque.java
    Adição de atributo arquivo e implementação de leitura

--- Estoque.java
+++ Estoque.java
@@ -1,7 +1,12 @@
 package softvendas;
 
-public class Estoque implements ControleDeDados{
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Estoque extends GrupoDeObjetos implements ControleDeDados{
     private Produto[] produtosEmEstoque;
+    private final java.io.File arquivoProdutos = new java.io.File("produtos.txt");
     
     @Override
     public void atualizarArquivo(){
@@ -10,6 +15,19 @@ public class Estoque implements ControleDeDados{
     
     @Override
     public void atualizarSistema(){
-        
+        if (arquivoProdutos.canRead()){
+            java.util.Scanner scannerReadInfo;
+            try {
+                produtosEmEstoque = new Produto[numItensNoArquivo() - 1];
+                scannerReadInfo = new java.util.Scanner(arquivoProdutos);
+                scannerReadInfo.nextLine();
+                int itemAtual = 0;
+                while(scannerReadInfo.hasNext()){
+                    produtosEmEstoque[itemAtual] = new Produto(scannerReadInfo.nextLine());
+                }    
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(RelatorioDeVendas.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
